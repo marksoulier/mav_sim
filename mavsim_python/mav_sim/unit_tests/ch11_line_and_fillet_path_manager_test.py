c53c03d5a488ebd4709601ca3b33308d606fc613
Greg Droge <greg.droge@usu.edu>
2024-01-08 09:51:41 -0700

    Update unit test code

--- mavsim_python/mav_sim/unit_tests/ch11_line_and_fillet_path_manager_test.py
+++ mavsim_python/mav_sim/unit_tests/ch11_line_and_fillet_path_manager_test.py
@@ -19,34 +19,6 @@ from mav_sim.message_types.msg_waypoints import MsgWaypoints
 
 PRECISION = 1e-4
 
-def gen_inHalfSpace_vars() -> List[Tuple[Dict[str, Any], Any]]:
-    """Generates test inputs and outputs for the inHlafSpace function."""
-    output: List[Tuple[Dict[str, Any], Any]] = []
-    # Base cases
-    inputs: Dict[str, Any] = {
-        "pos": np.zeros([3, 1], dtype=float),
-        "hs": HalfSpaceParams(
-            normal=np.zeros([3, 1], dtype=float),
-            point=np.zeros([3, 1], dtype=float),
-        ),
-    }
-    output.append((inputs, inHalfSpace(**inputs)))
-    # Exhaustive cases
-    for pos in itertools.product(np.arange(-10, 10.1, 5), repeat=3):
-        for normal in itertools.product(np.arange(-10, 10.1, 5), repeat=3):
-            for point in itertools.product(np.arange(-10, 10.1, 5), repeat=3):
-                inputs = {
-                    "pos": np.array(pos, dtype=float).reshape(-1, 1),
-                    "hs": HalfSpaceParams(
-                        normal=np.array(normal, dtype=float).reshape(-1, 1),
-                        point=np.array(point, dtype=float).reshape(-1, 1),
-                    ),
-                }
-                output.append((inputs, inHalfSpace(**inputs)))
-
-    return output
-
-
 def inHalfSpace_test(test_case: Tuple[Dict[str, Any], Any]) -> bool:
     """Tests the inHalfSpace function."""
     calculated_output = inHalfSpace(**test_case[0])
@@ -59,48 +31,6 @@ def inHalfSpace_test(test_case: Tuple[Dict[str, Any], Any]) -> bool:
         print(test_case[1])
         return False
     return True
-
-
-def gen_construct_line_vars() -> List[Tuple[Dict[str, Any], Any]]:
-    """Generates test inputs and outputs for the construct_line function."""
-    output: List[Tuple[Any, List[Any]]] = []
-    # Exhaustive cases
-    for root_pos in itertools.product(np.arange(-100, 100.1, 50), repeat=3):
-        for root_airspeed in np.arange(10, 50.1, 20):
-            for root_course in np.arange(-2 * np.pi, 2 * np.pi, 3):
-                waypoints = MsgWaypoints()
-                waypoints.add(
-                    ned=np.array(root_pos, dtype=float).reshape(-1, 1),
-                    airspeed=float(root_airspeed),
-                    course=float(root_course),
-                    cost=float(0),
-                    parent=int(-1),
-                    connect_to_goal=0,
-                )
-
-                for diff_pos in itertools.product(np.arange(-8, 19.1, 5), repeat=3):
-                    waypoints.add(
-                        ned=waypoints.ned[:, -1].reshape(-1, 1)
-                        + np.array(diff_pos, dtype=float).reshape(-1, 1),
-                        airspeed=float(
-                            root_airspeed + (np.array(diff_pos, dtype=float).sum() % 5)
-                        ),
-                        course=float(
-                            root_course + (np.array(diff_pos, dtype=float).sum() % 2)
-                        ),
-                        cost=float(waypoints.cost[-1] + 1.1),
-                        parent=int(waypoints.num_waypoints - 1),
-                        connect_to_goal=0,
-                    )
-                waypoints.connect_to_goal[-1] = 1
-                ptr = WaypointIndices()
-                lines = []
-                for _ in range(waypoints.num_waypoints - 1):
-                    lines.append(construct_line(waypoints=waypoints, ptr=ptr))
-                    ptr.increment_pointers(num_waypoints=waypoints.num_waypoints)
-            output.append((waypoints, lines))
-    return output
-
 
 def construct_line_test(test_case: Tuple[Any, List[Any]]) -> int:
     """Tests the construct_line function."""
@@ -174,55 +104,6 @@ def construct_line_test(test_case: Tuple[Any, List[Any]]) -> int:
             return ptr_it
         ptr.increment_pointers(num_waypoints=test_case[0].num_waypoints)
     return -1
-
-
-def gen_construct_fillet_line_vars() -> List[Tuple[Tuple[Any, Any], List[Any]]]:
-    """Generates test inputs and outputs for the construct_fillet_line function."""
-    output: List[Tuple[Tuple[Any, Any], List[Any]]] = []
-    # Exhaustive cases
-    for radius in np.arange(10, 50, 20):
-        for root_pos in itertools.product(np.arange(-100, 100.1, 50), repeat=3):
-            for root_airspeed in np.arange(10, 50.1, 20):
-                for root_course in np.arange(-2 * np.pi+.00001, 2 * np.pi-0.00001, 3):
-                    waypoints = MsgWaypoints()
-                    waypoints.add(
-                        ned=np.array(root_pos, dtype=float).reshape(-1, 1),
-                        airspeed=float(root_airspeed),
-                        course=float(root_course),
-                        cost=float(0),
-                        parent=int(-1),
-                        connect_to_goal=0,
-                    )
-
-                    for diff_pos in itertools.product(np.arange(-8, 19.1, 5), repeat=3):
-                        waypoints.add(
-                            ned=waypoints.ned[:, -1].reshape(-1, 1)
-                            + np.array(diff_pos, dtype=float).reshape(-1, 1),
-                            airspeed=float(
-                                root_airspeed
-                                + (np.array(diff_pos, dtype=float).sum() % 5)
-                            ),
-                            course=float(
-                                root_course
-                                + (np.array(diff_pos, dtype=float).sum() % 2)
-                            ),
-                            cost=float(waypoints.cost[-1] + 1.1),
-                            parent=int(waypoints.num_waypoints - 1),
-                            connect_to_goal=0,
-                        )
-                    waypoints.connect_to_goal[-1] = 1
-                    ptr = WaypointIndices()
-                    lines = []
-                    for _ in range(waypoints.num_waypoints - 1):
-                        lines.append(
-                            construct_fillet_line(
-                                waypoints=waypoints, ptr=ptr, radius=radius
-                            )
-                        )
-                        ptr.increment_pointers(num_waypoints=waypoints.num_waypoints)
-                output.append(((waypoints, radius), lines))
-    return output
-
 
 def construct_fillet_line_test(test_case: Tuple[Tuple[Any, Any], List[Any]]) -> int:
     """Tests the construct_fillet_line function."""
@@ -299,55 +180,6 @@ def construct_fillet_line_test(test_case: Tuple[Tuple[Any, Any], List[Any]]) -> 
         ptr.increment_pointers(num_waypoints=test_case[0][0].num_waypoints)
     return -1
 
-
-def gen_construct_fillet_circle_vars() -> List[Tuple[Tuple[Any, Any], List[Any]]]:
-    """Generates test inputs and outputs for the construct_fillet_circle function."""
-    output: List[Tuple[Tuple[Any, Any], List[Any]]] = []
-    # Exhaustive cases
-    for radius in np.arange(10, 50, 20):
-        for root_pos in itertools.product(np.arange(-100, 100.1, 50), repeat=3):
-            for root_airspeed in np.arange(10, 50.1, 20):
-                for root_course in np.arange(-2 * np.pi, 2 * np.pi, 3):
-                    waypoints = MsgWaypoints()
-                    waypoints.add(
-                        ned=np.array(root_pos, dtype=float).reshape(-1, 1),
-                        airspeed=float(root_airspeed),
-                        course=float(root_course),
-                        cost=float(0),
-                        parent=int(-1),
-                        connect_to_goal=0,
-                    )
-
-                    for diff_pos in itertools.product(np.arange(-8, 19.1, 5), repeat=3):
-                        waypoints.add(
-                            ned=waypoints.ned[:, -1].reshape(-1, 1)
-                            + np.array(diff_pos, dtype=float).reshape(-1, 1),
-                            airspeed=float(
-                                root_airspeed
-                                + (np.array(diff_pos, dtype=float).sum() % 5)
-                            ),
-                            course=float(
-                                root_course
-                                + (np.array(diff_pos, dtype=float).sum() % 2)
-                            ),
-                            cost=float(waypoints.cost[-1] + 1.1),
-                            parent=int(waypoints.num_waypoints - 1),
-                            connect_to_goal=0,
-                        )
-                    waypoints.connect_to_goal[-1] = 1
-                    ptr = WaypointIndices()
-                    lines = []
-                    for _ in range(waypoints.num_waypoints - 1):
-                        lines.append(
-                            construct_fillet_circle(
-                                waypoints=waypoints, ptr=ptr, radius=radius
-                            )
-                        )
-                        ptr.increment_pointers(num_waypoints=waypoints.num_waypoints)
-                output.append(((waypoints, radius), lines))
-    return output
-
-
 def construct_fillet_circle_test(test_case: Tuple[Tuple[Any, Any], List[Any]]) -> int:
     """Tests the construct_fillet_circle function."""
     ptr = WaypointIndices()
@@ -422,29 +254,6 @@ def construct_fillet_circle_test(test_case: Tuple[Tuple[Any, Any], List[Any]]) -
             return ptr_it
         ptr.increment_pointers(num_waypoints=test_case[0][0].num_waypoints)
     return -1
-
-
-def gen_test_archive() -> None:
-    """Makes the input/output archive of answers."""
-    archive: Dict[str, List[Tuple[Any, Any]]] = {}
-    # Gather test cases
-    print("Making inHalfSpace archive")
-    archive["inHalfSpace"] = gen_inHalfSpace_vars()
-    print("Making construct_line archive")
-    archive["construct_line"] = gen_construct_line_vars()
-    print("Making construct_fillet_line archive")
-    archive["construct_fillet_line"] = gen_construct_fillet_line_vars()
-    print("Making construct_fillet_circle archive")
-    archive["construct_fillet_circle"] = gen_construct_fillet_circle_vars()
-    # Save to archive
-    with open(
-        os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), "ch11_test_archive.pkl"
-        ),
-        "wb",
-    ) as file:
-        pickle.dump(archive, file)
-
 
 def run_all_tests() -> None:
     """Run all tests."""
